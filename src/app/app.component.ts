babe82983cb010c63281ca4ef1bdc1647765564b
unknown <jason_mulligan_77@hotmail.com>
2018-10-02 16:46:52 -0400

    Site Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cloud-rmmr-ui-angular';
+  title = 'Rate My Meeting Room';
 }
